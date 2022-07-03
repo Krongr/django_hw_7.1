99089a5566e1a0a389fccce85b06088419c2d427
Artem Nazarov <krongr.study@ya.ru>
2022-07-03 20:42:50 +0300

    Initial commit

---
+++ advertisements/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission
+from django.contrib.auth.models import User
+from advertisements.models import Advertisement
+
+
+class OwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.creator:
+            return True
+        _user = User.objects.get(username=request.user)
+        return _user.is_superuser
+
+class Draft(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if obj.status == "DRAFT":
+            return request.user == obj.creator
+        return True
---
+++ advertisements/views.py
@@ -0,0 +1,26 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.permissions import Draft, OwnerOrAdmin
+from advertisements.filters import AdvertisementFilter
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["retrieve",]:
+            return [Draft()]
+        if self.action in ["create",]:
+            return [IsAuthenticated()]
+        if self.action in ["update", "partial_update", "destroy",]:
+            return [OwnerOrAdmin()]
+        return []
